9acf71a2363f9be75645f0a155fd71093d6b7e95
dnkhanh45 <yoshokarui@gmail.com>
2024-10-06 16:53:17 +0700

    spp

--- scripts/train_lct_ot_newc_allnonscalinglayernorm_ffhq.sh
+++ scripts/train_lct_ot_newc_allnonscalinglayernorm_ffhq.sh
@@ -1,3 +1,25 @@
+#!/bin/bash -e
+#SBATCH --job-name=sbatch-1                                                                 # create a short name for your job
+#SBATCH --output=/lustre/scratch/client/vinai/users/thienlt3/khanhdn10/lct/logs/mbpp%A.out     # create a output file
+#SBATCH --error=/lustre/scratch/client/vinai/users/thienlt3/khanhdn10/lct/logs/mbpp%A.err      # create a error file
+#SBATCH --partition=research                                                                # choose partition
+#SBATCH --gpus-per-node=2
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-gpu=40GB
+#SBATCH --nodes=1
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-017
+#SBATCH --ntasks=1
+#SBATCH --mail-type=begin                                                                   # send email when job begins
+#SBATCH --mail-type=end                                                                     # send email when job ends
+#SBATCH --mail-type=fail                                                                    # send email when job fails
+#SBATCH --mail-user=v.khanhdn10@vinai.io
+
+# conda env
+module purge
+module load python/miniconda3/miniconda3
+eval "$(conda shell.bash hook)"
+conda activate /lustre/scratch/client/vinai/users/thienlt3/envs/khanhdn10/lct
+
 export MASTER_PORT=10125
 
 CUDA_VISIBLE_DEVICES=0,1 torchrun --nnodes=1 --rdzv_endpoint 0.0.0.0:$MASTER_PORT --nproc_per_node=2 train_cm_latent.py \
@@ -16,7 +38,7 @@ CUDA_VISIBLE_DEVICES=0,1 torchrun --nnodes=1 --rdzv_endpoint 0.0.0.0:$MASTER_POR
         --start-scales 10 \
         --end-scales 640 \
         --noise-sampler ict \
-        --global-batch-size $((128*2)) \
+        --global-batch-size $((256*2)) \
         --epochs $((1400*1)) \
         --lr 1e-4 \
         --num-sampling 8 \
