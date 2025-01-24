ec89e24a065e8ad7080905bb01bb4c405923bcf6
aengusng8 <aengus.ng8@gmail.com>
2025-01-24 21:12:18 +0700

    g

---
+++ scripts/train_lct_dit.sh
@@ -0,0 +1,36 @@
+# # export MASTER_PORT=10120
+PREFIX=/home/khanhdn10/repo/lct
+
+CUDA_VISIBLE_DEVICES=0,1 torchrun --nnodes=1 --rdzv_endpoint 0.0.0.0:10120 --nproc_per_node=2 train_cm_latent.py \
+        --exp celeb_dit_repa  \
+        --datadir $PREFIX/dataset/ \
+        --dataset latent_celeb256 \
+        --results-dir results/ \
+        --image-size 32 \
+        --num-in-channels 4 \
+        --num-classes 0 \
+        --weight-schedule ict \
+        --loss-norm cauchy \
+        --target-ema-mode adaptive \
+        --start-ema 0.95 \
+        --scale-mode progressive \
+        --start-scales 10 \
+        --end-scales 640 \
+        --noise-sampler ict \
+        --global-batch-size $((32*2)) \
+        --epochs $((1400*1)) \
+        --lr 1e-4 \
+        --num-sampling 8 \
+        --num-channels 128 \
+        --num-head-channels 64 \
+        --num-res-blocks 4 \
+        --resblock-updown \
+        --ict \
+        --max-grad-norm 100.0 \
+        --model-type DiT-B/2 \
+        --channel-mult 1,2,3,4 \
+        --attention-resolutions 16,8 \
+        --normalize-matrix celeb256_stat.npy \
+        --use-diffloss \
+        --ot-hard \
+        --c-by-loss-std \
