8930fe9f27d1bae55c85320adba94f886c8acf08
aengusng8 <aengus.ng8@gmail.com>
2025-01-27 01:24:53 +0700

    g

--- scripts/train_lct_ot_celeb.sh
+++ scripts/train_lct_ot_celeb.sh
@@ -43,13 +43,14 @@ torchrun --nnodes=1 --nproc_per_node=$NUM_GPUS train_cm_latent.py \
         --c-by-loss-std \
         --linear-act relu \
         --no-scale \
+        --plot-every 5 \
+        --use-repa \
         --projector-dim 2048 \
         --enc-type dinov2-vit-b \
         --encoder-depth 4 \
-        --plot-every 5 \
-        --use-repa \
         --repa-lamb 0.5 \
         --use-bf16 \
+        --umt \
         --vae-type $PREFIX_CKPT/stabilityai/sd-vae-ft-ema # mit-han-lab/dc-ae-f32c32-in-1.0
         # --compile \
         # --resume
