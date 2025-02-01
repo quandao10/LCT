8b2bc3a39ee2a230f9fbb262341a3eb9b5412c85
aengusng8 <aengus.ng8@gmail.com>
2025-02-02 02:18:42 +0700

    gg

--- scripts/train_lct_dit_repa.sh
+++ scripts/train_lct_dit_repa.sh
@@ -1,4 +1,4 @@
-MASTER_PORT=11248
+MASTER_PORT=11249
 DATASET=/lustre/scratch/client/movian/research/users/anhnd72/datasets/LCT/latent_celeb256
 NUM_GPUS=$1
 
@@ -7,7 +7,7 @@ NUM_GPUS=$1
 
 # CUDA_VISIBLE_DEVICES=4,5,6,7 
 CUDA_VISIBLE_DEVICES=0 torchrun --nnodes=1 --nproc_per_node=$NUM_GPUS --master_port $MASTER_PORT train_cm_latent.py \
-        --exp baseline_repa1.0_diff2.0  \
+        --exp baseline_repa2.0_diff1.0  \
         --datadir $DATASET/ \
         --dataset latent_celeb256 \
         --results-dir results/ \
@@ -44,8 +44,8 @@ CUDA_VISIBLE_DEVICES=0 torchrun --nnodes=1 --nproc_per_node=$NUM_GPUS --master_p
         --projector-dim 2048 \
         --enc-type dinov2-vit-b \
         --encoder-depth 4 \
-        --repa-lamb 1.0 \
-        --diff-lamb 2.0 \
+        --repa-lamb 2.0 \
+        --diff-lamb 1.0 \
         --z_dims 768 \
         --ckpt-every 100 \
         --use-repa \
