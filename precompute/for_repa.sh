6b9ca0013f6230cbfc25a3a7cced74fd87c41b8d
aengusng8 <aengus.ng8@gmail.com>
2025-02-01 18:25:45 +0700

    gg

--- precompute/for_repa.sh
+++ precompute/for_repa.sh
@@ -2,8 +2,8 @@ image_dir="/lustre/scratch/client/movian/research/users/khanhdn10/datasets/celeb
 dataset_name="celeba_256"
 output_dir="/lustre/scratch/client/movian/research/users/anhnd72/datasets/LCT/latent_celeb256"
 repa_enc_type="dinov2-vit-b"
-batch_size=128
-num_workers=32
+batch_size=64
+num_workers=8
 vae_type="stabilityai/sd-vae-ft-ema"
 
 python -m precompute.for_repa --image_dir $image_dir --dataset_name $dataset_name --output_dir $output_dir --repa_enc_type $repa_enc_type --batch_size $batch_size --num_workers $num_workers --vae_type $vae_type
