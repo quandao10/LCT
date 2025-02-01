6ba8d068abb36132635061324f238cd48e5420d4
aengusng8 <aengus.ng8@gmail.com>
2025-02-02 01:53:20 +0700

    gg

--- scripts/train_lct_dit_repa.sh
+++ scripts/train_lct_dit_repa.sh
@@ -6,7 +6,7 @@ NUM_GPUS=$1
 # CUDA_VISIBLE_DEVICES=0 torchrun --nnodes=1 --nproc_per_node=1 train_cm_latent.py \
 
 # CUDA_VISIBLE_DEVICES=4,5,6,7 
-CUDA_VISIBLE_DEVICES=2,3 torchrun --nnodes=1 --nproc_per_node=$NUM_GPUS train_cm_latent.py \
+CUDA_VISIBLE_DEVICES=2,3 torchrun --nnodes=1 --nproc_per_node=$NUM_GPUS --master_port 10117 train_cm_latent.py \
         --exp debug  \
         --datadir $DATASET/ \
         --dataset latent_celeb256 \
