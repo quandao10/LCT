b82f2e97badb18a3bb5e611a7cb3f92194625000
aengusng8 <aengus.ng8@gmail.com>
2025-02-01 12:41:22 +0700

    gg

--- precompute/ssl_feat.py
+++ precompute/ssl_feat.py
@@ -31,22 +31,21 @@ def precompute_ssl_feat(args):
 
         ####################### REPA #######################
         ssl_feat = None
-        if args.use_repa:
-            with torch.no_grad():
-                target = x.clone().detach()
-                raw_image = target / vae.config.scaling_factor
-                raw_image = vae.decode(raw_image.to(dtype=vae.dtype)).sample.float()
-                raw_image = (raw_image * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-                import ipdb; ipdb.set_trace()
-                ssl_feat = []
-                # with torch.autocast(device_type='cuda', dtype=__dtype):
-                with torch.autocast(device_type='cuda'):
-                    for encoder, encoder_type, arch in zip(encoders, encoder_types, architectures):
-                        raw_image_ = preprocess_raw_image(raw_image, encoder_type)
-                        z = encoder.forward_features(raw_image_)
-                        if 'mocov3' in encoder_type: z = z = z[:, 1:] 
-                        if 'dinov2' in encoder_type: z = z['x_norm_patchtokens']
-                        ssl_feat.append(z.detach())
+        with torch.no_grad():
+            target = x.clone().detach()
+            raw_image = target / vae.config.scaling_factor
+            raw_image = vae.decode(raw_image.to(dtype=vae.dtype)).sample.float()
+            raw_image = (raw_image * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+            import ipdb; ipdb.set_trace()
+            ssl_feat = []
+            # with torch.autocast(device_type='cuda', dtype=__dtype):
+            with torch.autocast(device_type='cuda'):
+                for encoder, encoder_type, arch in zip(encoders, encoder_types, architectures):
+                    raw_image_ = preprocess_raw_image(raw_image, encoder_type)
+                    z = encoder.forward_features(raw_image_)
+                    if 'mocov3' in encoder_type: z = z = z[:, 1:] 
+                    if 'dinov2' in encoder_type: z = z['x_norm_patchtokens']
+                    ssl_feat.append(z.detach())
     
 def parse_args():
     parser = argparse.ArgumentParser()
