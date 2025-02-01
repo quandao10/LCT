ee8dc29ce57e1faec554dea3cb14d52331fddc1c
aengusng8 <aengus.ng8@gmail.com>
2025-02-01 18:33:55 +0700

    gg

--- scripts/train_lct_dit_repa.sh
+++ scripts/train_lct_dit_repa.sh
@@ -5,7 +5,7 @@ NUM_GPUS=$1
 # CUDA_VISIBLE_DEVICES=0,1 torchrun --nnodes=1 --rdzv_endpoint 0.0.0.0:10120 --nproc_per_node=2 train_cm_latent.py \
 # CUDA_VISIBLE_DEVICES=0 torchrun --nnodes=1 --nproc_per_node=1 train_cm_latent.py \
 
-torchrun --nnodes=1 --nproc_per_node=$NUM_GPUS train_cm_latent.py \
+CUDA_VISIBLE_DEVICES=1 torchrun --nnodes=1 --nproc_per_node=$NUM_GPUS train_cm_latent.py \
         --exp baseline_repa0.5  \
         --datadir $PREFIX/ \
         --results-dir results/ \
