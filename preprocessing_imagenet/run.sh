9ff0b8d84624aa478513cc75dce99fadcf47780d
aengusng8 <aengus.ng8@gmail.com>
2025-02-03 15:31:59 +0700

    gg

--- preprocessing_imagenet/run.sh
+++ preprocessing_imagenet/run.sh
@@ -1,10 +1,16 @@
-# # 1. Convert raw ImageNet data to a ZIP archive at 256x256 resolution
-# python preprocessing_imagenet/dataset_tools.py convert \
-#     --source=/lustre/scratch/client/vinai/users/anhnd72/datasets/real_imagenet/train \
-#     --dest=/lustre/scratch/client/movian/research/users/anhnd72/datasets/LCT/real_imagenet_256/images \
-#     --resolution=256x256 --transform=center-crop-dhariwal
+MODE=$1
+echo "Mode: $MODE"
 
-# 2. Convert the pixel data to VAE latents
-python preprocessing_imagenet/dataset_tools.py encode \
-    --source=/lustre/scratch/client/movian/research/users/anhnd72/datasets/LCT/real_imagenet_256/images \
-    --dest=/lustre/scratch/client/movian/research/users/anhnd72/datasets/LCT/real_imagenet_256/vae-sdvae-ft-ema
+if [ $MODE -eq 1 ]; then
+    # # 1. Convert raw ImageNet data to a ZIP archive at 256x256 resolution
+    python preprocessing_imagenet/dataset_tools.py convert \
+        --source=/lustre/scratch/client/vinai/users/anhnd72/datasets/real_imagenet/train \
+        --dest=/lustre/scratch/client/movian/research/users/anhnd72/datasets/LCT/real_imagenet_256/images \
+        --resolution=256x256 --transform=center-crop-dhariwal
+
+elif [ $MODE -eq 2 ]; then
+    # 2. Convert the pixel data to VAE latents
+    python preprocessing_imagenet/dataset_tools.py encode \
+        --source=/lustre/scratch/client/movian/research/users/anhnd72/datasets/LCT/real_imagenet_256/images \
+        --dest=/lustre/scratch/client/movian/research/users/anhnd72/datasets/LCT/real_imagenet_256/vae-sdvae-ft-ema
+fi
