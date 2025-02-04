44e5e9e421d762bf570fc517fadd01cdf87581a5
aengusng8 <aengus.ng8@gmail.com>
2025-02-05 05:26:10 +0700

    gg

--- scripts/train_lct_dit_repa.sh
+++ scripts/train_lct_dit_repa.sh
@@ -15,7 +15,7 @@ GRAD_NORM=100.0
 # MODEL_TYPE=DiT-B/2
 MODEL_TYPE=LightningDiT-B/2
 START_SCALES=10 # 10 20 40 80 160 320 640
-START_SCALES=80
+START_SCALES=160
 
 # CUDA_VISIBLE_DEVICES=0,1 torchrun --nnodes=1 --rdzv_endpoint 0.0.0.0:10120 --nproc_per_node=2 train_cm_latent.py \
 # CUDA_VISIBLE_DEVICES=0 torchrun --nnodes=1 --nproc_per_node=1 train_cm_latent.py \
