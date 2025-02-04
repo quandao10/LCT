64b1edcae0580839430c8ee3d074f2516c84d2f9
aengusng8 <aengus.ng8@gmail.com>
2025-02-04 21:57:37 +0700

    gg

--- play.py
+++ play.py
@@ -2,6 +2,11 @@
 import os
 
 directory = "/lustre/scratch/client/movian/research/users/anhnd72/datasets/LCT/real_imagenet_256/images"
+all_files = []
 for root, dirs, files in os.walk(directory):
     for file in files:
-        print(os.path.join(root, file))
+        all_files.append(os.path.join(root, file))
+
+print(f"\033[33mFound {len(all_files)} files\033[0m")
+
+import ipdb; ipdb.set_trace()
