c42f4c2b0f63a74f1255cc2fbbc5dd0dd178939d
aengusng8 <aengus.ng8@gmail.com>
2025-02-04 19:00:43 +0700

    gg

--- precompute/for_repa.sh
+++ precompute/for_repa.sh
@@ -1,3 +1,4 @@
+
 image_dir="/lustre/scratch/client/movian/research/users/khanhdn10/datasets/celeba_256_png"
 dataset_name="celeba_256"
 output_dir="/lustre/scratch/client/movian/research/users/anhnd72/datasets/LCT/latent_celeb256"
@@ -6,6 +7,6 @@ batch_size=128
 num_workers=64
 vae_type="stabilityai/sd-vae-ft-ema"
 run_VAE=False
-run_SSL=true
+run_SSL=True
 
-python -m precompute.for_repa --image_dir $image_dir --dataset_name $dataset_name --output_dir $output_dir --repa_enc_type $repa_enc_type --batch_size $batch_size --num_workers $num_workers --vae_type $vae_type
+python -m precompute.for_repa --image_dir $image_dir --dataset_name $dataset_name --output_dir $output_dir --repa_enc_type $repa_enc_type --batch_size $batch_size --num_workers $num_workers --vae_type $vae_type   
