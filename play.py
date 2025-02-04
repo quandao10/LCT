b9ed42e9dae80bc190e556aae1a731bd9b9ba555
aengusng8 <aengus.ng8@gmail.com>
2025-02-04 21:57:18 +0700

    gg

--- play.py
+++ play.py
@@ -1,51 +1,7 @@
-class TimestepEmbedder(nn.Module):
-    """
-    Embeds scalar timesteps into vector representations.
-    Same as DiT.
-    """
+# find all files in the directory
+import os
 
-    def __init__(self, hidden_size: int, frequency_embedding_size: int = 256) -> None:
-        super().__init__()
-        self.frequency_embedding_size = frequency_embedding_size
-        self.mlp = nn.Sequential(
-            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
-            nn.SiLU(),
-            nn.Linear(hidden_size, hidden_size, bias=True),
-        )
-
-    @staticmethod
-    def timestep_embedding(
-        t: torch.Tensor, dim: int, max_period: int = 10000
-    ) -> torch.Tensor:
-        """
-        Create sinusoidal timestep embeddings.
-        Args:
-            t: A 1-D Tensor of N indices, one per batch element. These may be fractional.
-            dim: The dimension of the output.
-            max_period: Controls the minimum frequency of the embeddings.
-        Returns:
-            An (N, D) Tensor of positional embeddings.
-        """
-        # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
-        half = dim // 2
-        freqs = torch.exp(
-            -math.log(max_period)
-            * torch.arange(start=0, end=half, dtype=torch.float32)
-            / half
-        ).to(device=t.device)
-
-        args = t[:, None].float() * freqs[None]
-        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
-
-        if dim % 2:
-            embedding = torch.cat(
-                [embedding, torch.zeros_like(embedding[:, :1])], dim=-1
-            )
-
-        return embedding
-
-    @torch.compile
-    def forward(self, t: torch.Tensor) -> torch.Tensor:
-        t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
-        t_emb = self.mlp(t_freq)
-        return t_emb
+directory = "/lustre/scratch/client/movian/research/users/anhnd72/datasets/LCT/real_imagenet_256/images"
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        print(os.path.join(root, file))
